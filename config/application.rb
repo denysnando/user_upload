a0b863ab89b04ea58288815c70b907e5b04e4cbf
Denys <denysnando@gmail.com>
2020-10-19 10:19:34 -0300

    fix: add eager_load_paths
    n

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,12 @@ module UserUpload
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.autoload_paths += %W[#{config.root}/lib]
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('lib/**/*.rb')
+    config.eager_load_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib/**/*.rb')
+    config.eager_load_paths << Rails.root.join('app', 'serializers')
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
