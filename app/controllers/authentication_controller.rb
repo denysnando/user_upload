5e1502470b2f9156e3cd770172c064ac6ba70aea
Denys <denysnando@gmail.com>
2020-10-18 23:44:46 -0300

    fix: remove all @

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,7 +4,7 @@ class AuthenticationController < ApplicationController
   def login
     user = User.find_by_email(params[:email])
     if user&.authenticate(password: params[:password])
-      token = JsonWebToken.encode(user_id: @user.id)
+      token = JsonWebToken.encode(user_id: user.id)
       time = Time.zone.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
                      name: user.name,
