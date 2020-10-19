d586628637519c8b3336dc30e98b9ec45e55640b
Denys <denysnando@gmail.com>
2020-10-18 23:43:28 -0300

    fix: remove instance @user

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,12 +2,12 @@ class AuthenticationController < ApplicationController
   before_action :authorize_request, except: :login
 
   def login
-    @user = User.find_by_email(params[:email])
-    if @user&.authenticate(password: params[:password])
+    user = User.find_by_email(params[:email])
+    if user&.authenticate(password: params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.zone.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
-                     name: @user.name,
+                     name: user.name,
                      user_id: user.id }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
