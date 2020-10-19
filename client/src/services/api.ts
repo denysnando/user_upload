318229b01eaa03148f69ca93f562087af482dd48
Denys <denysnando@gmail.com>
2020-10-19 09:35:12 -0300

    fix: changed api.ts production path

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 import { store } from '~/store';
 
-export const baseURL = process.env.baseURL || 'http://localhost:3000';
+export const baseURL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
 
 const api = axios.create({
   baseURL,
